cab1a544c6527f4b05971feeb99a72278e8fc453
Emeka Nwachukwu <enwachukwu@byteworks.com.ng>
2021-10-24 09:35:55 +0100

    project setup

---
+++ src/app/search/search.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SearchRoutingModule } from './search-routing.module';
+import { SearchComponent } from './search.component';
+import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './components/list/list.component';
+
+
+@NgModule({
+  declarations: [
+    SearchComponent,
+    DetailsComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule,
+    SearchRoutingModule
+  ]
+})
+export class SearchModule { }
